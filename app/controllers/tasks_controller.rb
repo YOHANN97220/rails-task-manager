c0194593b3effafa076b61b3eafc91f9eac9dad4
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-16 14:39:38 +0100

    action new and create added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,35 @@
 class TasksController < ApplicationController
+  before_action :get_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @task = Task.all
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    get_task
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.create(task_params)
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+    get_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+private
+
+  def task_params
+    params(:task).permit(:title, :details)
+  end
+
+  def get_task
+    @task = Task.find(params[:id])
   end
 end
